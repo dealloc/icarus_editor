c4ab17d5808cbe957ddb59320cb96655da38d538
Wannes Gennar <wannes.gennar@gmail.com>
2022-12-30 23:37:21 +0100

    add core library

---
+++ src/icarus_editor_core/example/icarus_editor_core_example.dart
@@ -0,0 +1,6 @@
+import 'package:icarus_editor_core/icarus_editor_core.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ src/icarus_editor_core/lib/icarus_editor_core.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library icarus_editor_core;
+
+export 'src/icarus_editor_core_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
