6bfa3bbcff05caffc6c82da3914b6811b54bb5fd
Wannes Gennar <wannes.gennar@gmail.com>
2023-01-06 21:17:57 +0100

    remove unused tabs

--- src/icarus_editor/lib/widgets/character/character_overview.dart
+++ src/icarus_editor/lib/widgets/character/character_overview.dart
@@ -3,34 +3,17 @@ import 'package:fluent_ui/fluent_ui.dart';
 import '../../services/icarus_character.dart';
 import 'character_stats.dart';
 
-class CharacterOverview extends StatefulWidget {
+class CharacterOverview extends StatelessWidget {
   final IcarusCharacter character;
 
   const CharacterOverview({super.key, required this.character});
-
-  @override
-  State<CharacterOverview> createState() => _CharacterOverviewState();
-}
-
-class _CharacterOverviewState extends State<CharacterOverview> {
-  int activeTab = 0;
 
   @override
   Widget build(BuildContext context) {
     return ColoredBox(
       color: FluentTheme.of(context).scaffoldBackgroundColor,
       child: Center(
-        child: TabView(
-          currentIndex: activeTab,
-          onChanged: (value) => setState(() => activeTab = value),
-          closeButtonVisibility: CloseButtonVisibilityMode.never,
-          tabs: [
-            Tab(
-              text: const Text('Stats'),
-              body: CharacterStats(character: widget.character),
-            ),
-          ],
-        ),
+        child: CharacterStats(character: character),
       ),
     );
   }
